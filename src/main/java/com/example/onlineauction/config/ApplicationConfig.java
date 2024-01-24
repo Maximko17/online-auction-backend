9534682f72a94d04c337bdd6604bc3549fabda37
Sukhodolets Maxim <maxim.sukhodolets@gmail.com>
2024-01-24 18:18:23 +0300

    Added minio service

--- src/main/java/com/example/onlineauction/config/ApplicationConfig.java
+++ src/main/java/com/example/onlineauction/config/ApplicationConfig.java
@@ -1,14 +1,28 @@
 package com.example.onlineauction.config;
 
+import com.example.onlineauction.props.MinioProperties;
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final MinioProperties minioProperties;
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getUrl())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
     }
 }
